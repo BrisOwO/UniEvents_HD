e5e56528d845cd5f71e7b9a4a4acb350402af96b
BrisOwO <briseidag0311@gmail.com>
2025-11-11 22:53:05 -0600

    changes rutas

--- Src/Login/RegistrarUsuario.php
+++ Src/Login/RegistrarUsuario.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../ConexionPHP/conexion.php';
 header('Content-Type: text/html; charset=utf-8');
 session_start();
 
@@ -134,7 +135,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Nombre de la ventana (parte del navegador) -->
     <title>Registro UniEvents+</title>
-    <link rel="stylesheet" href="../../public/styles/styles.css">
+    <link rel="stylesheet" href="/public/styles/style.css">
     <!-- Iconos de las casillas -->
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
 </head>
